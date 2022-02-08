88c8a0aca048bcf9eeee759a9221dd4d004549fa
santhosh312 <sanet3136@gmail.com>
2022-02-08 04:37:05 +0000

    todos get functionality

--- app.js
+++ app.js
@@ -35,6 +35,18 @@ const hasPriorityAndStatusProperties = (requestQuery) => {
   );
 };
 
+const hasCategoryAndStatusProperties = (requestQuery) => {
+  return (
+    requestQuery.category !== undefined && requestQuery.status !== undefined
+  );
+};
+
+const hasPriorityAndCategoryProperties = (requestQuery) => {
+  return (
+    requestQuery.priority !== undefined && requestQuery.category !== undefined
+  );
+};
+
 const hasPriorityProperty = (requestQuery) => {
   return requestQuery.priority !== undefined;
 };
@@ -42,3 +54,102 @@ const hasPriorityProperty = (requestQuery) => {
 const hasStatusProperty = (requestQuery) => {
   return requestQuery.status !== undefined;
 };
+
+const hasCategoryProperty = (requestQuery) => {
+  return requestQuery.category !== undefined;
+};
+
+app.get("/todos/", async (request, response) => {
+  let data = null;
+  let getTodosQuery = "";
+  const { search_q = "", priority, status, category } = request.query;
+
+  switch (true) {
+    case hasPriorityAndStatusProperties(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND status = '${status}'
+        AND priority = '${priority}';`;
+      break;
+    case hasCategoryAndStatusProperties(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND status = '${status}'
+        AND category = '${category}';`;
+      break;
+    case hasPriorityAndCategoryProperties(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND category = '${category}'
+        AND priority = '${priority}';`;
+      break;
+
+    case hasPriorityProperty(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND priority = '${priority}';`;
+      break;
+    case hasStatusProperty(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND status = '${status}';`;
+      break;
+    case hasCategoryProperty(request.query):
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND category = '${category}';`;
+      break;
+    default:
+      getTodosQuery = `
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%';`;
+  }
+
+  data = await database.all(getTodosQuery);
+  const updatedData = data.map((item) => ({
+    id: item.id,
+    todo: item.todo,
+    category: item.category,
+    priority: item.priority,
+    status: item.status,
+    dueDate: item.due_date,
+  }));
+
+  response.send(updatedData);
+});
+
+module.exports = app;
